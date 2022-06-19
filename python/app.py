73e5891851a3f0a8dae9467ea1d6ebb0ea1f0a1b
darkfaderx <darkfaderx@hotmail.com>
2022-06-19 12:37:53 +1000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello New World v1.3 again 2!"
+    return "Hello New World v1.3 again 3!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
